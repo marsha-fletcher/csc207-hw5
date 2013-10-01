72be4fb37866bfee9c47cf0ce3a7ac227f4430dd
KittNika <kittnika@gmail.com>
2013-10-01 16:13:23 -0500

    Added preconditions, postconditions, and documentation

--- src/edu/grinnell/csc207/fletcher1/hw5/UshahidiExtensions.java
+++ src/edu/grinnell/csc207/fletcher1/hw5/UshahidiExtensions.java
@@ -6,6 +6,9 @@ import edu.grinnell.glimmer.ushahidi.*;
 
 public class UshahidiExtensions {
 
+	/* Preconditions: No additional.
+	 * Postconditions: Prints relevant information about the given UshahidiIncident. */
+	
 	public static void printIncident(PrintWriter pen, UshahidiIncident event){
 		pen.println("Incident #: "+ event.getTitle()
 				+ "\n  " + event.getDescription()
@@ -15,10 +18,17 @@ public class UshahidiExtensions {
 				+ "\n");
 	} // printIncident(PrintWriter, UshahidiIncident)
 	
+	/* Preconditions:
+	 * Postconditions: */
+	
 	public static UshahidiIncidentList UshahidiIncidentList() {
 		// STUB
 		return null;
 	} // UshahidiIncidentList()
+	
+	/* Preconditions: Incident IDs must be positive integers.
+	 * Postconditions: Prints the UshahidiIncidents with the highest and lowest IDs
+	 * from the given UshahidiClient. */
 	
 	public static void extremeIncidents(UshahidiClient client) throws Exception {
 		int currentId, largest, smallest;
@@ -29,24 +39,25 @@ public class UshahidiExtensions {
 		UshahidiIncident current;
 		PrintWriter pen = new PrintWriter(System.out, true);
 		
-		for (client.hasMoreIncidents();;) {
-			current = client.nextIncident();
-			currentId = current.getId();
-			if (currentId > largest) {
-				largest = currentId;
+		for (client.hasMoreIncidents();;) { // while there are incidents in the client
+			current = client.nextIncident(); // get the next incident
+			currentId = current.getId(); // get the next incident's id
+			if (currentId > largest) { // if this incident's id is the largest so far, set it
+				largest = currentId;   // to the incident big
 				big = current;
 			} // if
-			if (currentId < smallest) {
-				smallest = currentId;
+			if (currentId < smallest) { // if this incident's id is the smallest so far, set it
+				smallest = currentId;   // to the incident small
 				small = current;
 			} // if
 			
-			if (! client.hasMoreIncidents()) {
+			if (! client.hasMoreIncidents()) { // break before reaching the end of the client
 				break;
 			}
 			
 		} // for
 		
+		// print the incidents
 		pen.println("Lowest ID\n");
 		printIncident(pen, small);
 		pen.println("Highest ID\n");
@@ -54,6 +65,10 @@ public class UshahidiExtensions {
 		
 	} // extremeIncidents(UshahidiClient)
 
+	/* Preconditions: No additional.
+	 * Postconditions: Prints the UshahidiIncidents between the given start and end dates from
+	 * the given UshahidiClient. */
+	
 	public void identifyIncidents(UshahidiClient client, GregorianCalendar start, 
 			GregorianCalendar end) throws Exception {
 		
@@ -63,9 +78,9 @@ public class UshahidiExtensions {
 		GregorianCalendar currentDate;
 		PrintWriter pen = new PrintWriter(System.out, true);
 		
-		for (client.hasMoreIncidents();;) {
-			current = client.nextIncident();
-			currentDate = (GregorianCalendar) current.getDate();
+		for (client.hasMoreIncidents();;) { // while there are incidents in the client
+			current = client.nextIncident(); // get the next incident
+			currentDate = (GregorianCalendar) current.getDate(); // get the next incident's date
 			
 			if (currentDate == start) {
 				afterStart = true;
@@ -75,15 +90,21 @@ public class UshahidiExtensions {
 				afterEnd = true;
 			}
 			
+			// print the incidents between the start and end dates
 			if (afterStart && afterEnd == false) {
 				printIncident(pen, current);
 			}
 			
+			// break after we reach the end date
 			if (afterEnd) {
 				break;
 			}
 		}
 	} // identifyIncidents(UshahidiClient, GregorianCalendar, GregorianCalendar)
+	
+	/* Preconditions:
+	 * Postconditions: Returns a list of the UshahidiIncidents between the given start and end
+	 * dates from the given UshahidiClient. */
 	
 	public static UshahidiIncidentList selectIncidents(UshahidiClient client, 
 			GregorianCalendar start, GregorianCalendar end) throws Exception {
@@ -94,9 +115,9 @@ public class UshahidiExtensions {
 		GregorianCalendar currentDate;
 		UshahidiIncidentList found = UshahidiIncidentList();
 		
-		for (client.hasMoreIncidents();;) {
-			current = client.nextIncident();
-			currentDate = (GregorianCalendar) current.getDate();
+		for (client.hasMoreIncidents();;) { // while there are incidents in the client
+			current = client.nextIncident(); // get the next incident
+			currentDate = (GregorianCalendar) current.getDate(); // get the next incident's date
 			
 			if (currentDate == start) {
 				afterStart = true;
@@ -106,10 +127,12 @@ public class UshahidiExtensions {
 				afterEnd = true;
 			}
 			
+			// add the incidents between the dates to the list to be returned
 			if (afterStart && afterEnd == false) {
 				found.addIncident(current);
 			}
 			
+			// break after we reach the end date
 			if (afterEnd) {
 				break;
 			}
