e0b68f17cc21673a072d5e6f2861e2104083a179
KittNika <kittnika@gmail.com>
2013-10-01 15:18:52 -0500

    Most stubs filled out

--- src/edu/grinnell/csc207/fletcher1/hw5/UshahidiExtensions.java
+++ src/edu/grinnell/csc207/fletcher1/hw5/UshahidiExtensions.java
@@ -1,6 +1,7 @@
 package edu.grinnell.csc207.fletcher1.hw5;
 
 import java.io.PrintWriter;
+import java.util.GregorianCalendar;
 import edu.grinnell.glimmer.ushahidi.*;
 
 public class UshahidiExtensions {
@@ -10,26 +11,75 @@ public class UshahidiExtensions {
 				+ "\n  " + event.getDescription()
 				+ "\n  Location: "+ event.getLocation()
 				+ "\n  Status: (" + event.getMode() + event.getActive() 
-				+ event.getVerified() + ")");
+				+ event.getVerified() + ")"
+				+ "\n");
 	} // printIncident(PrintWriter, UshahidiIncident)
 	
-	public static void UshahidiIncidentList() {
+	public static UshahidiIncidentList UshahidiIncidentList() {
 		
-	}
+		return null;
+	} // UshahidiIncidentList()
 	
-	public static void extremeIncidents(UshahidiClient client) {
-		int currentId;
-		UshahidiIncidentList();
+	public static void extremeIncidents(UshahidiClient client) throws Exception {
+		int currentId, largest, smallest;
+		smallest = 0;
+		largest = 0;
+		UshahidiIncident small;
+		UshahidiIncident big;
+		UshahidiIncident current;
+		PrintWriter pen = new PrintWriter(System.out, true);
 		
 		for (client.hasMoreIncidents();;) {
-			
-			
-			
-		}
-	}
-	
-	public static void identifyIncidents() {
+			current = client.nextIncident();
+			currentId = current.getId();
+			if (currentId > largest) {
+				largest = currentId;
+				big = current;
+			} // if
+			if (currentId > smallest) {
+				smallest = currentId;
+				small = current;
+			} // if
+		} // for
 		
-	}
+		pen.println("Lowest ID\n");
+		printIncident(pen, small);
+		pen.println("Highest ID\n");
+		printIncident(pen, big);
+		
+	} // extremeIncidents(UshahidiClient)
+
+	public void identifyIncidents(UshahidiClient client, GregorianCalendar start, 
+			GregorianCalendar end) throws Exception {
+		
+		boolean afterStart = false;
+		boolean afterEnd = false;
+		UshahidiIncident current;
+		GregorianCalendar currentDate;
+		PrintWriter pen = new PrintWriter(System.out, true);
+		
+		for (client.hasMoreIncidents();;) {
+			current = client.nextIncident();
+			currentDate = (GregorianCalendar) current.getDate();
+			
+			if (currentDate == start) {
+				afterStart = true;
+			}
+			
+			if (currentDate == end) {
+				afterEnd = true;
+			}
+			
+			if (afterStart && afterEnd == false) {
+				printIncident(pen, current);
+			}
+		}
+	} // identifyIncidents()
+	
+	public static UshahidiIncident[] selectIncidents(GregorianCalendar start, 
+			GregorianCalendar end) {
+		
+		return null;
+	} // identifyIncidents(GregorianCalendar, GregorianCalender)
 	
 } // UshahidiExtensions
